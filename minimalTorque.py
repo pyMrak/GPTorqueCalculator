e7cbca334093c78676d7bcd4b8ae017880fa93c5
CORP\andmra2 <andrej.mrak2@hidria.com>
2021-10-22 11:21:13 +0200

    Fixed self. bug.

--- minimalTorque.py
+++ minimalTorque.py
@@ -9,7 +9,7 @@ class graphVar():
     COLORS = ['#007f00', '#dc0000', '#000000', '#00007f', '#dbdb00', '#00c8c8', '#00c800', '#0000fa', '#ff9900',
               '#00ff7f', '#009e4e', '#00fef2', '#00a39a', '#ff00ff']
 
-    def __init__(self, Dv, Dk, kn, k, minForce=1000):
+    def __init__(self, Dv, Dk, kn, kF, minForce=None):
         self.variable = {}
         self.variable['M'] = linspace(7, 18, 100)  # moment privitja [Nm]
         self.variable['Dv'] = Dv  # nazivni premer vijaka [mm]
@@ -29,9 +29,12 @@ class graphVar():
         self.cont = 'M'
         self.disc = 'kf'
         self.dicsVar = array([0.15, 0.2, 0.25, 0.3])
-        self.k = k  #VW 414: 69.2  #PSA 166: 43.56;41.592 #[N/K]
+        self.kF = kF  #[N/K] #VW 414: 69.2  #PSA 166: 43.56;41.592
         self.Tok = 22  # [C]
-        self.minForce = minForce  # [N]
+        if minForce is None:
+            self.minForce = Dk**2*pi*7
+        else:
+            self.minForce = minForce  # [N]
 
     def setCont(self, var, low, high):
         self.variable[var] = linspace(low, high, 100)
@@ -45,19 +48,19 @@ class graphVar():
         self.variable[var] = val
 
     def calculateF(self):
-        self.variable["F"] = Graph.variable['M'] / (
-                Graph.variable['Dv'] / 2 * tan(sin(Graph.variable['kn'] / (pi * Graph.variable['Dv'])) +
-                                               arctan(2 * Graph.variable['kf'] / (3 ** (1 / 2)))) +
-                Graph.variable['kf'] * Graph.variable['Dk'] / 2) * 1000
+        self.variable["F"] = self.variable['M'] / (
+                self.variable['Dv'] / 2 * tan(sin(self.variable['kn'] / (pi * self.variable['Dv'])) +
+                                               arctan(2 * self.variable['kf'] / (3 ** (1 / 2)))) +
+                self.variable['kf'] * self.variable['Dk'] / 2) * 1000
 
     def calculateM(self):
-        self.variable["M"] = Graph.variable['F'] * (
-                    Graph.variable['Dv'] / 2 * tan(sin(Graph.variable['kn'] / (pi * Graph.variable['Dv'])) +
-                                                   arctan(2 * Graph.variable['kf'] / (3 ** (1 / 2)))) +
-                    Graph.variable['kf'] * Graph.variable['Dk'] / 2) / 1000
+        self.variable["M"] = self.variable['F'] * (
+                    self.variable['Dv'] / 2 * tan(sin(self.variable['kn'] / (pi * self.variable['Dv'])) +
+                                                   arctan(2 * self.variable['kf'] / (3 ** (1 / 2)))) +
+                    self.variable['kf'] * self.variable['Dk'] / 2) / 1000
 
     def calculateT(self):
-        self.variable["T"] = (self.variable["F"] - self.minForce) / self.k + self.Tok
+        self.variable["T"] = (self.variable["F"] - self.minForce) / self.kF + self.Tok
 
     def updateVariables(self, variable):
         if variable == "M":
@@ -81,11 +84,11 @@ class graphVar():
         ax1.set_xlabel(xLabel)
         ax1.set_ylabel(yLabel)
         plt.grid(which='major')
-        for i in range(len(Graph.dicsVar)):
+        for i in range(len(self.dicsVar)):
             self.variable[self.disc] = self.dicsVar[i]
             if self.updateVariables(var) is not None:
-                ax1.plot(Graph.variable[Graph.cont], Graph.variable[var], color=Graph.COLORS[1 - i],
-                         label=Graph.disc + '=' + str(Graph.variable[Graph.disc]) + self.units[Graph.disc])
+                ax1.plot(self.variable[self.cont], self.variable[var], color=self.COLORS[1 - i],
+                         label=self.disc + '=' + str(self.variable[self.disc]) + self.units[self.disc])
         ax1.legend(loc=1)
         plt.show()
 
@@ -95,7 +98,7 @@ class graphVar():
 
 
 if __name__ == "__main__":
-    Graph = graphVar(Dv=10, Dk=7, kn=1, k=69.2, minForce=1000)
+    Graph = graphVar(Dv=10, Dk=7, kn=1, kF=69.2, minForce=1000)
     Graph.setCont('kf', 0.2, 0.3)
     Graph.setDisc('F', [7500, 10000])  #
     Graph.plotVariable("M", "lala")
@@ -119,7 +122,7 @@ if __name__ == "__main__":
 #                                  Graph.variable['kf']*Graph.variable['Dk']/2)*1000
 #     # print(len(force))
 #     # print(force)
-#     Tmax = (force - minForce) / k + Tok
+#     Tmax = (force - minForce) / kF + Tok
 #     ax1.plot(Graph.variable[Graph.cont], Tmax, color=Graph.COLORS[1 - i],
 #              label=Graph.disc + '=' + str(Graph.variable[Graph.disc]) + 'Nm')
 #
