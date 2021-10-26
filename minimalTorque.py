0f760979d573446f2533d51eed26b8c5bbfbbc95
CORP\andmra2 <andrej.mrak2@hidria.com>
2021-10-26 09:35:44 +0200

    Fixed calculating bugs. Tests added.

--- minimalTorque.py
+++ minimalTorque.py
@@ -11,13 +11,14 @@ class graphVar():
 
     def __init__(self, Dv, Dk, kn, kF, minForce=None):
         self.variable = {}
+        self.defaultVal = {}
         self.variable['M'] = linspace(7, 18, 100)  # moment privitja [Nm]
         self.variable['Dv'] = Dv  # nazivni premer vijaka [mm]
         self.variable['Dk'] = Dk  # testni premer konusa [mm]
         self.variable['kn'] = kn  # korak navoja [mm]
-        self.variable['kf'] = 0.2  # koeficient trenja [/]
+        self.variable['kf'] = 0.25  # koeficient trenja [/]
         self.variable['T'] = 22  # maximalna temperatura [°C]
-        self.variable['F'] = 1000  # sila privitja [N]
+        self.variable['F'] = 0  # sila privitja [N]
         self.units = {"M": "Nm",
                       "Dv": "mm",
                       "Dk": "mm",
@@ -35,61 +36,121 @@ class graphVar():
             self.minForce = Dk**2*pi*7
         else:
             self.minForce = minForce  # [N]
+        self.defaultVal['M'] = 15  # moment privitja [Nm]
+        self.defaultVal['Dv'] = Dv  # nazivni premer vijaka [mm]
+        self.defaultVal['Dk'] = Dk  # testni premer konusa [mm]
+        self.defaultVal['kn'] = kn  # korak navoja [mm]
+        self.defaultVal['kf'] = 0.25  # koeficient trenja [/]
+        self.defaultVal['T'] = 22  # maximalna temperatura [°C]
+        self.defaultVal['F'] = 0#self.minForce  # sila privitja [N]
 
     def setCont(self, var, low, high):
+        if self.disc != self.cont:
+            self.variable[self.cont] = self.defaultVal[self.cont]
         self.variable[var] = linspace(low, high, 100)
         self.cont = var
 
+
+
     def setDisc(self, var, val):
+        if self.disc != self.cont:
+            self.variable[self.disc] = self.defaultVal[self.disc]
         self.dicsVar = array(val)
         self.disc = var
+
+
+    def setVar(self, var):
+        self.var = var
 
     def set(self, var, val):
         self.variable[var] = val
 
-    def calculateF(self):
+    def getMissingVar(self):
+        if self.cont != "M" and self.disc != "M" and self.var != "M":
+            return "M"
+        elif self.cont != "kf" and self.disc != "kf" and self.var != "kf":
+            return "kf"
+
+    def calculateF(self, includeT=False, minF=0):
+        if includeT:
+            TF = (self.variable['T']-self.Tok)*self.kF
+        else:
+            TF = 0
         self.variable["F"] = self.variable['M'] / (
                 self.variable['Dv'] / 2 * tan(sin(self.variable['kn'] / (pi * self.variable['Dv'])) +
                                                arctan(2 * self.variable['kf'] / (3 ** (1 / 2)))) +
-                self.variable['kf'] * self.variable['Dk'] / 2) * 1000
+                self.variable['kf'] * self.variable['Dk'] / 2) * 1000 - TF - minF
 
-    def calculateM(self):
-        self.variable["M"] = self.variable['F'] * (
+    def calculateM(self, Fmin=None):
+        if Fmin:
+            minF = Fmin
+        else:
+            minF = 0
+        print(minF, self.variable['F'])
+        self.variable["M"] = (self.variable['F'] + minF + (self.variable['T']-self.Tok)*self.kF) * (
                     self.variable['Dv'] / 2 * tan(sin(self.variable['kn'] / (pi * self.variable['Dv'])) +
                                                    arctan(2 * self.variable['kf'] / (3 ** (1 / 2)))) +
                     self.variable['kf'] * self.variable['Dk'] / 2) / 1000
 
-    def calculateT(self):
-        self.variable["T"] = (self.variable["F"] - self.minForce) / self.kF + self.Tok
+    def calculateT(self, includeFmin=False):
+        if includeFmin:
+            minF = self.minForce
+        else:
+            minF = 0
+        self.variable["T"] = (self.variable["F"] - minF) / self.kF + self.Tok
 
-    def updateVariables(self, variable):
+    def calculateFfromT(self):
+         self.variable["F"] = (self.variable["T"] - self.Tok)*self.kF
+
+
+
+    def updateVariables(self, variable, minF):
         if variable == "M":
-            self.calculateM()
+            if self.disc != "F" and self.cont != "F":
+                self.calculateM(Fmin=self.minForce)
+            else:
+                self.calculateM()
         elif variable == "F":
-            self.calculateF()
-        elif variable == "T":
-            if self.disc != "M" and self.cont != "M":
-                return None
-            elif self.disc != "F" and self.cont != "F":
+            if self.cont == "T":
+                self.calculateFfromT()
+            elif self.disc == "T":
+                self.calculateF(True)
+            else:
                 self.calculateF()
-            self.calculateT()
+        elif variable == "T":
+            if self.disc != "F" and self.cont != "F":
+                self.calculateF(includeT=False)
+            if self.cont == "M":
+                self.calculateF(minF=minF)
+            if self.cont == "F":
+                self.calculateT(False)
+            else:
+                self.calculateT(True)
         return self.variable[variable]
 
-    def plotVariable(self, var, title=None):
+    def plotVariable(self, title=None, includeFmin=False):
         graph, ax1 = plt.subplots(figsize=(8, 5))  # figure()
         if title is not None:
             plt.title(title)
         xLabel = "{0} [{1}]".format(self.cont, self.units[self.cont])
-        yLabel = "{0} [{1}]".format(var, self.units[var])
+        yLabel = "{0} [{1}]".format(self.var, self.units[self.var])
         ax1.set_xlabel(xLabel)
         ax1.set_ylabel(yLabel)
         plt.grid(which='major')
         for i in range(len(self.dicsVar)):
-            self.variable[self.disc] = self.dicsVar[i]
-            if self.updateVariables(var) is not None:
-                ax1.plot(self.variable[self.cont], self.variable[var], color=self.COLORS[1 - i],
-                         label=self.disc + '=' + str(self.variable[self.disc]) + self.units[self.disc])
-        ax1.legend(loc=1)
+            if self.cont == "M" and self.disc == "F":
+                minF = self.dicsVar[i]
+                lab = str(self.dicsVar[i])
+            else:
+                self.variable[self.disc] = self.dicsVar[i]
+                minF = 0
+                lab = str(self.variable[self.disc])
+            if self.updateVariables(self.var, minF=minF) is not None:
+                ax1.plot(self.variable[self.cont], self.variable[self.var], color=self.COLORS[1 - i],
+                         label=self.disc + '=' + lab + self.units[self.disc])
+        specialPair = ("T", "F")
+        if self.var not in specialPair or self.cont not in specialPair:
+            ax1.legend(loc=1)
         plt.show()
 
 
@@ -98,10 +159,28 @@ class graphVar():
 
 
 if __name__ == "__main__":
-    Graph = graphVar(Dv=10, Dk=7, kn=1, kF=69.2, minForce=1000)
-    Graph.setCont('kf', 0.2, 0.3)
-    Graph.setDisc('F', [7500, 10000])  #
-    Graph.plotVariable("M", "lala")
+    # tests
+    testDict = {"T": {"cont": [22, 150], "disc": [22, 100, 150]},
+                "F": {"cont": [0, 10000], "disc": [7000, 10000, 14000]},
+                "M": {"cont": [0, 20], "disc": [10, 15, 20]},
+                "kf": {"cont": [0.1, 0.5], "disc": [0.1, 0.3, 0.5]},
+                }
+    testGroups = {"T": {"kf": ["M", "F"], "F": ["M"], "M": ["F"]},
+                  "F": {"kf": ["M", "T"], "T": ["M"], "M": ["T"]},
+                  "kf": {"F": ["M"], "T": ["F", "M"], "M": ["F", "T"]},
+                  "M": {"kf": ["T", "F"], "F": ["T"], "T": ["F"]},
+                  }
+    #testGroups = {"kf": {"T": ["M"]}}
+    for cont in testGroups:
+        for disc in testGroups[cont]:
+            for var in testGroups[cont][disc]:
+                print("Ploting\tcont: {0}\n\t\tdiscr: {1}\n\t\tvar: {2}".format(cont, disc, var))
+                Graph = graphVar(Dv=10, Dk=7, kn=1, kF=69.2, minForce=1000)
+                Graph.setCont(cont, *testDict[cont]["cont"])
+                Graph.setDisc(disc, testDict[disc]["disc"])  #
+                Graph.setVar(var)
+                Graph.plotVariable("{0}({1}, {2})".format(var, cont, disc))
+                print("---------------------------\n\n")
 
 # graph, ax1 = plt.subplots(figsize=(8, 5))  # figure()
 # # plt.hold(True)
